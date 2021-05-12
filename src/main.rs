bab691fc887f1d048929e0fde514f12119ab618a
laffed <roarklaffed@gmail.com>
2021-05-12 17:18:03 -0500

    setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#![feature(decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
+
+use rocket_contrib::json::Json;
+use serde_json::Value;
+
+#[get("/status")]
+fn status() -> &'static str {
+    "clock status"
+}
+
+#[get("/status")]
+fn clocker() -> &'static str {
+    "clocking"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![status, clocker])
+        .launch();
 }
